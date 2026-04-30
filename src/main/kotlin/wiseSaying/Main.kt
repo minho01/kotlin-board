e0b2ab16d1c6a0ebcc9c25e752fe02f90cdcf519
minho <minho01@hs.ac.kr>
2026-04-30 11:08:32 +0900

    1

---
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -0,0 +1,16 @@
+package wiseSaying
+
+fun main() {
+
+    println("== 명언 앱 ==")
+
+    while (true){
+        print("명령) ")
+
+        val input = readln()
+        if(input == "종료"){
+            println("프로그램을 종료합니다.")
+            break
+        }
+    }
+}
